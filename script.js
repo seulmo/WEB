5a26b4bfc0799ff4d444432f3949e5806c8a6f34
seulmo <mog56723@gmail.com>
2025-12-16 22:48:56 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function() {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
+
+
